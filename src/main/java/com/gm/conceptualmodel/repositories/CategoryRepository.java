320f1c1c09591e1df68952c9554359c4662e72a5
Gregory Moser <gdc.moser@hotmail.com>
2021-02-06 16:16:03 +0000

    CategoryRepository and CategoryService Created

---
+++ src/main/java/com/gm/conceptualmodel/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.gm.conceptualmodel.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gm.conceptualmodel.domain.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer>{
+	
+	
+}
