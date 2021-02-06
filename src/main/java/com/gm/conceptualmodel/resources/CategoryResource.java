942f374e478e3026c036677342091c02d5033fef
Gregory Moser <gdc.moser@hotmail.com>
2021-02-06 15:33:44 +0000

    Testing the first domain class - Category

--- src/main/java/com/gm/conceptualmodel/resources/CategoryResource.java
+++ src/main/java/com/gm/conceptualmodel/resources/CategoryResource.java
@@ -1,15 +1,28 @@
 package com.gm.conceptualmodel.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gm.conceptualmodel.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST is working!";
+	public List<Category> listar() {
+		
+		Category cat1 = new Category(1, "Computing");
+		Category cat2 = new Category(2, "Office");
+		
+		List <Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 }
