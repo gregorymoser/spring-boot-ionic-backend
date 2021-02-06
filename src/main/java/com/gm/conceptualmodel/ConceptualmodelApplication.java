49bc635d3d00a9caac9da383d1617e1a7d36610a
Gregory Moser <gdc.moser@hotmail.com>
2021-02-06 15:10:39 +0000

    Project Created

---
+++ src/main/java/com/gm/conceptualmodel/ConceptualmodelApplication.java
@@ -0,0 +1,12 @@
+package com.gm.conceptualmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceptualmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConceptualmodelApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gm/conceptualmodel/ConceptualmodelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm.conceptualmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConceptualmodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
