a766c8f38b8831bd1c1870fb8f9e6c53448f4699
Gregory Moser <gdc.moser@hotmail.com>
2021-02-06 16:35:54 +0000

    Creating instantiation operation

--- src/main/java/com/gm/conceptualmodel/ConceptualmodelApplication.java
+++ src/main/java/com/gm/conceptualmodel/ConceptualmodelApplication.java
@@ -1,12 +1,31 @@
 package com.gm.conceptualmodel;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ConceptualmodelApplication {
+import com.gm.conceptualmodel.domain.Category;
+import com.gm.conceptualmodel.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class ConceptualmodelApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ConceptualmodelApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Computing");
+		Category cat2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
