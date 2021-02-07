e74807baebd576adbdb2ba96f31526a08b7e973d
Gregory Moser <gdc.moser@hotmail.com>
2021-02-07 12:03:59 +0000

    Update JsonIgnore

--- src/main/java/com/gm/conceptualmodel/domain/Customer.java
+++ src/main/java/com/gm/conceptualmodel/domain/Customer.java
@@ -14,8 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.gm.conceptualmodel.domain.enums.CustomerType;
 
 @Entity
@@ -31,7 +30,6 @@ public class Customer implements Serializable {
 	private String pps;
 	private Integer type;
 
-	@JsonManagedReference
 	@OneToMany(mappedBy = "customer")
 	private List<Address> addresses = new ArrayList<>();
 
@@ -39,7 +37,7 @@ public class Customer implements Serializable {
 	@CollectionTable(name = "PHONE")
 	private Set<String> phones = new HashSet<>();
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy = "customer")
 	private List<Request> requests = new ArrayList<>();
 
